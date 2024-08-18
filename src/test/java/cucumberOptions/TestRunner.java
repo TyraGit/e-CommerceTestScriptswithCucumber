ec80b270a92e889933d79c94d80e555afecb29dc
AthirahGit <athirahazirah96@gmail.com>
2024-08-18 13:20:55 +0800

    Update TestRunner.java

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/java/features", 
-				glue="stepDefinitions", 
+				glue={"stepDefinitions"}, 
 				monochrome=true, 
 				plugin = {"pretty", "html:target/cucumber-reports.html"})
 public class TestRunner extends AbstractTestNGCucumberTests {
